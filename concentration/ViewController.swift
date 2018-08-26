81e7404c38822f43ddeab00ba69d7062c36f125b
mcgraths7 <mcgrath.steven7@gmail.com>
2018-08-26 08:12:02 -0400

    Initial Commit

---
+++ concentration/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  concentration
+//
+//  Created by Steven McGrath on 8/26/18.
+//  Copyright © 2018 Steven McGrath. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
